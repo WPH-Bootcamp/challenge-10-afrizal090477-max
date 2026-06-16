d685b75c7c5697409613796a7616c1e7fbd4f923
Riza Yakub <afrizal.090477@gmail.com>
2026-06-16 22:29:55 +0700

    feat: complete Navbar menu and clean bug

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,20 +1,24 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useAuthStore } from "@/store/auth.store";
 import NavDesktop from "./navbar/NavDekstop";
 import NavMobile from "./navbar/NavMobile";
 
 export default function Navbar() {
   const router = useRouter();
+  const pathname = usePathname();
+  
   const token = useAuthStore((state) => state.token);
   const user = useAuthStore((state) => state.user);
   const clearAuth = useAuthStore((state) => state.clearAuth);
-
   const [isScrolled, setIsScrolled] = useState(false);
+  const isHomePage = pathname === "/";
 
   useEffect(() => {
+    if (!isHomePage) return;
+
     const handleScroll = () => {
       if (window.scrollY > 80) {
         setIsScrolled(true);
@@ -22,20 +26,22 @@ export default function Navbar() {
         setIsScrolled(false);
       }
     };
-    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    window.addEventListener("scroll", handleScroll, { passive: true });
     return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  }, [isHomePage]); 
+  const shouldShowSolidNavbar = !isHomePage || isScrolled;
 
   return (
     <nav
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out ${
-        isScrolled
+        shouldShowSolidNavbar
           ? "h-16 md:h-20 bg-white shadow-md text-slate-900 border-b border-slate-100"
           : "h-16 md:h-20 bg-transparent text-white"
       }`}
     >
       <NavDesktop 
-        isScrolled={isScrolled}
+        isScrolled={shouldShowSolidNavbar}
         isLoggedIn={!!token} 
         user={user}
         logout={clearAuth}
@@ -43,7 +49,7 @@ export default function Navbar() {
       />
 
       <NavMobile 
-        isScrolled={isScrolled}
+        isScrolled={shouldShowSolidNavbar}
         isLoggedIn={!!token} 
         user={user}
         logout={clearAuth}
